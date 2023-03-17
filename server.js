3743aaf973359a6b9c1e97cf36d2c9a169e81b02
blitzen23 <michaelthe23@gmail.com>
2023-03-17 22:08:56 +0900

    commit

--- server.js
+++ server.js
@@ -23,8 +23,18 @@ connectDB();
 // }
 
 // Init Middleware
-app.use(express.json({ extended: false }));
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var bodyParser = require('body-parser');
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
