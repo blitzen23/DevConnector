68dcc9ace7816a4325507957b571453ff525b845
blitzen23 <michaelthe23@gmail.com>
2023-03-17 21:48:29 +0900

    commit

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ const app = express();
 connectDB();
 
 // Your code
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, 'client', 'build', 'index.html'),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   const path = require('path');
+//   app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, 'client', 'build', 'index.html'),
+//       function (err) {
+//         if (err) {
+//           res.status(500).send(err);
+//         }
+//       }
+//     );
+//   });
+// }
 
 // Init Middleware
 app.use(express.json({ extended: false }));
